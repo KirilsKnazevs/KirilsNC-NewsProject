af608cd371977a9cf26d915971be1d48d2d5d1f5
Kirils <kirils.knazevs@gmail.com>
2022-10-11 16:46:42 +0100

    6. PATCH /api/articles/:article_id

--- models/articles_models.js
+++ models/articles_models.js
@@ -1,3 +1,5 @@
+const { isNumberLiteral } = require("@babel/types");
+const { number } = require("yargs");
 const db = require("../db/connection");
 
 exports.selectArticleById = (id) => {
@@ -9,4 +11,22 @@ exports.selectArticleById = (id) => {
       }
       return result.rows[0];
     });
+};
+
+exports.updateArticleById = (article_id, votes) => {
+  if (votes && typeof votes !== "number") {
+    return Promise.reject({ status: 400, msg: "Invalid input" });
+  } else {
+    return db
+      .query(
+        `UPDATE articles SET votes=votes+$1 WHERE article_id=$2 RETURNING*;`,
+        [votes, article_id]
+      )
+      .then(({ rows: [article] }) => {
+        if (article === undefined) {
+          return Promise.reject({ status: 404, msg: "Id not found" });
+        }
+        return article;
+      });
+  }
 };
