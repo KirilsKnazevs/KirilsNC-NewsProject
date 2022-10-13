b2e9af1d5c66b34f4d57307b4ee963506f8d0ad9
Kirils <kirils.knazevs@gmail.com>
2022-10-13 13:41:55 +0100

    add GET /api/articles/:article_id/comments endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -286,7 +286,7 @@ describe("6. 08-GET_articles /api/articles", () => {
   });
 });
 
-describe.only("6. 09-GET_api_articles_article.id_comments /api/articles/:article_id/comments", () => {
+describe("6. 09-GET_api_articles_article.id_comments /api/articles/:article_id/comments", () => {
   test("status:200, responds with an array of test comments for given article_id objects", () => {
     return request(app)
       .get(`/api/articles/9/comments`)
